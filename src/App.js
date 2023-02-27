a26d5807b1c383ffee68652558b4d2e86972c6fd
אביגיל דביר <214104465@mbydomain>
2023-02-27 13:35:26 +0200

    starting react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Routes, Route, NavLink} from "react-router-dom"
+import Login from "./pages/login/Login";
+import Home  from "./pages/home/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Router>
+    <nav>
+      <NavLink to="/">Koogle</NavLink>
+      <NavLink to="/login">Sign In</NavLink>
+    </nav>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/login" element={<Login/>} />
+    </Routes>
+   </Router>
   );
 }
 
